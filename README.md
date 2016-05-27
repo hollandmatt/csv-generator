0fc210f524237cf9f5759fda3d7b1eca16cb8a93
Matt Holland <matth@stytch.com>
2016-05-27 14:16:18 -0700

    create a script to generate random CSV data

---
+++ README.md
@@ -0,0 +1,16 @@
+# CSV generator
+
+A script to generate random data in CSV format.
+
+## Usage
+
+1. Install Python - `brew install python` on a Mac
+1. `./csv_generator.py <rows> <columns> <output file>`
+
+## Options
+
+- `rows` - How many rows of data to generate
+- `columns` - How many columns the data should have
+- `output file` - Where to write the results to
+
+Use `./csv_generator.py --help` to see this on the command line.
---
+++ csv_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import csv, argparse, random, string
+
+parser = argparse.ArgumentParser(description='Generate a CSV of random data.')
+parser.add_argument('rows', help='Number of rows of data to generate')
+parser.add_argument('columns', help='Number of columns of data to generate')
+parser.add_argument('filename', help='Where to write the generated data')
+
+args = parser.parse_args()
+
+DATUM_LENGTH = 6
+
+headers = []
+for col in range(0, int(args.columns)):
+    rand = ''.join(random.choice(string.ascii_lowercase) for _ in range(DATUM_LENGTH))
+    headers.append('Header-' + rand)
+
+output = csv.writer(open(args.filename, 'wb'))
+output.writerow(headers)
+
+for row in range(0, int(args.rows)):
+    data = []
+    for col in range(0, int(args.columns)):
+        data.append(''.join(random.choice(string.ascii_lowercase) for _ in range(DATUM_LENGTH)))
+    output.writerow(data)
